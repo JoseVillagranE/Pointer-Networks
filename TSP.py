2a7006ae3f8611ca466c971e581ff8fd01979869
José Villagrán <josevillagran18@gmail.com>
2020-04-21 23:58:54 -0400

    Add forward for only one example

--- TSP.py
+++ TSP.py
@@ -18,11 +18,73 @@ import functools
 from time import time
 
 from TSPDataset import TSPDataset
-#sys.path.insert(0, "D:/MaterialU/12 semestre/Trabajo_dirigido/Diego/codigo/PointerNetwork")
 from pointer_network import PointerNet, PointerNetLoss
 
+def PreProcessOutput(outp):
+    
+    outp = np.array([outp[i] - 1 for i in range(outp.shape[0]) if outp[i] != 0])
+    return outp
 
-def training(model, train_ds, eval_ds, cudaAvailable, batchSize=1, attention_size=128, beam_width=2, lr=1e-3, clip_norm=5.0,
+
+def eval_model(model, eval_ds, cudaAvailable, batchSize=1):
+    model.eval()
+    if cudaAvailable:
+         use_cuda = True
+         torch.cuda.device(0)
+         model = model.cuda()
+    else:
+        use_cuda = False
+        
+    countAcc = 0
+    eval_dl = DataLoader(eval_ds, num_workers=0, batch_size=batchSize)
+    for b_eval_inp, b_eval_inp_len, b_eval_outp_in, b_eval_outp_out, b_eval_outp_len in eval_dl:
+        
+        b_eval_inp = Variable(b_eval_inp)
+        b_eval_outp_in = Variable(b_eval_outp_in)
+        b_eval_outp_out = Variable(b_eval_outp_out)
+        
+        if use_cuda:
+            b_eval_inp = b_eval_inp.cuda()
+            b_eval_inp_len = b_eval_inp_len.cuda()
+            b_eval_outp_in = b_eval_outp_in.cuda()
+            b_eval_outp_out = b_eval_outp_out.cuda()
+            b_eval_outp_len = b_eval_outp_len.cuda()
+        
+        align_score = model(b_eval_inp, b_eval_inp_len, b_eval_outp_in, b_eval_outp_len)
+        align_score = align_score.cpu().detach().numpy()
+        idxs = np.argmax(align_score[0], axis=1)
+        b_eval_outp_out = b_eval_outp_out.cpu().detach().numpy()
+        b_eval_outp_out = b_eval_outp_out.squeeze()
+        idxs = PreProcessOutput(idxs)
+        labels = PreProcessOutput(b_eval_outp_out)
+        
+        if functools.reduce(lambda i, j: i and j, map(lambda m, k: m==k, idxs, labels), True):
+            countAcc += 1
+    
+    Acc = countAcc/eval_ds.__len__()
+    print("The Accuracy of the model is: {}".format(Acc))
+
+
+def plot_one_tour(model, example, cudaAvailable):
+    
+    model.eval()
+    inp, inp_len, outp_in, outp_out, outp_len = example
+    inp_t = Variable(torch.from_numpy(np.array([inp])))
+    inp_len = torch.from_numpy(inp_len)
+    outp_in = Variable(torch.from_numpy(np.array([outp_in])))
+    outp_out = Variable(torch.from_numpy(outp_out))
+    align_score = model(inp_t, inp_len, outp_in, outp_len)
+    align_score = align_score.detach().numpy()
+    idxs = np.argmax(align_score[0], axis=1)
+    idxs = PreProcessOutput(idxs)
+    
+    inp = inp[1:, :]
+    plt.figure(figsize=(10,10))
+    plt.plot(inp[:,0], inp[:,1], 'o')
+    for i in range(idxs.shape[0]-1):
+        plt.plot(inp[[idxs[i], idxs[i+1]],0], inp[[idxs[i], idxs[i+1]], 1], 'k-')
+    
+def training(model, train_ds, eval_ds, cudaAvailable, batchSize=10, attention_size=128, beam_width=2, lr=1e-3, clip_norm=5.0,
              weight_decay=0.1, nepoch = 30, model_file="PointerModel.pt", freqEval=5):
     
   t0 = time()
@@ -114,17 +176,20 @@ if __name__ == "__main__":
 #    cudaAvailable = False
     seq_len = 5
     num_layers = 1
+    encoder_input_size = 2
     rnn_hidden_size = 32
     
-    model = PointerNet("LSTM", True, num_layers, 2, rnn_hidden_size, 0.0)
+    model = PointerNet("LSTM", True, num_layers, encoder_input_size, rnn_hidden_size, 0.0)
     
-    train_ds = TSPDataset(train_filename, seq_len)
-    eval_ds = TSPDataset(val_filename, seq_len)
+    train_ds = TSPDataset(train_filename, seq_len, lineCountLimit=1000)
+    eval_ds = TSPDataset(val_filename, seq_len, lineCountLimit=-1)
     
     print("Train data size: {}".format(len(train_ds)))
     print("Eval data size: {}".format(len(eval_ds)))
     
-#    model.load_state_dict(torch.load("PointerModel.pt"))
-    training(model, train_ds, eval_ds, cudaAvailable, nepoch=100)
-#    eval_model(model, train_ds, cudaAvailable)    
+    model.load_state_dict(torch.load("TSPModel.pt"))
+#    TrainingLoss, EvalLoss = training(model, train_ds, eval_ds, cudaAvailable, nepoch=100)
+#    eval_model(model, eval_ds, cudaAvailable)
+    example = eval_ds.__getitem__(0)
+    plot_one_tour(model, example, cudaAvailable)    
 #    example = train_ds.__getitem__(0)
